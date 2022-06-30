4a2c80f658fd9165405d79cbc1c2a2de092a1686
zantaclaus <tarzuto@gmail.com>
2022-06-30 21:14:04 +0700

    create pages

---
+++ src/pages/belief/header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BeliefHeader = () => {
+  return <div>Belief</div>;
+};
+
+export default BeliefHeader;
---
+++ src/pages/dress/header/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const DressHeader = () => {
+  return <div>Dress</div>;
+};
+
+export default DressHeader;
