2c2a88b2dd1c37395455a7cbf7f23d537d4066b8
zantaclaus <tarzuto@gmail.com>
2022-07-03 16:01:44 +0700

    fix: image path error

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,7 +2,7 @@ import hero from './hero.jpeg';
 import temple from './temple.jpeg';
 import wadRongKhun from './wadRongKhun.png';
 import banDam from './banDam.png';
-import banDamBg from './banDamBG.png';
+import banDamBg from './banDamBg.png';
 import doiDinDang from './doiDinDang.png';
 import doiDinDangBg from './doiDinDangBG.png';
 import watSueaLongTen from './watSueaLongTen.png';
